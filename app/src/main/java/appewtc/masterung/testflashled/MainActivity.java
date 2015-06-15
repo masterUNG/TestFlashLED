da25f7a8afa99ac3028c19ef038976cbaf79e479
masterUNG <phrombutr@gmail.com>
2015-06-15 18:41:46 +0700

    Class

--- app/src/main/java/appewtc/masterung/testflashled/MainActivity.java
+++ app/src/main/java/appewtc/masterung/testflashled/MainActivity.java
@@ -1,37 +1,70 @@
 package appewtc.masterung.testflashled;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import ioio.lib.util.BaseIOIOLooper;
+import ioio.lib.util.android.IOIOActivity;
+
+public class MainActivity extends IOIOActivity{
+
+    private ImageView greenImageView, orangeImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindWidget();
+
+        greenController();
+
+    }   // onCreate
+
+    class Looper extends BaseIOIOLooper {
+
+        
+
+    }   //Looper class
+
+    private void greenController() {
+
+        greenImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    greenImageView.setImageAlpha(50);
+                    soundEffect1();
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    greenImageView.setImageAlpha(255);
+                    soundEffect2();
+                }
+
+                return true;
+            }
+        });
+
+    }   //greenController
+
+    private void soundEffect2() {
+        MediaPlayer sound2Player = MediaPlayer.create(getBaseContext(), R.raw.phonton2);
+        sound2Player.start();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void soundEffect1() {
+        MediaPlayer sound1Player = MediaPlayer.create(getBaseContext(), R.raw.phonton1);
+        sound1Player.start();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void bindWidget() {
+        greenImageView = (ImageView) findViewById(R.id.imageView);
+        orangeImageView = (ImageView) findViewById(R.id.imageView2);
     }
-}
+
+
+}   // Main Class
